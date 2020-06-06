a665505eaa89565f04e1b82ca1967a1509fa7871
microprediction <info@3za.org>
2020-06-06 10:49:19 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="getjson",
-    version="0.0.1",
+    version="0.0.2",
     description="Conventions used at MicroPrediction.Org",
     long_description=README,
     long_description_content_type="text/markdown",
