f971098c662bb9a621f6c66315cdc94422700524
microprediction <info@3za.org>
2020-06-06 10:51:37 -0400

    fixed dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["backoff"],
+    install_requires=["backoff","requests"],
     entry_points={
         "console_scripts": [
             "microconventions=microconventions.__main__:main",
