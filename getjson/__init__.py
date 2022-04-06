43c6a83e6183137f3f05071b345f6c78276fb7d5
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-04-06 09:02:35 -0400

    Update __init__.py

--- getjson/__init__.py
+++ getjson/__init__.py
@@ -1,8 +1,4 @@
 from getjson.requests_implementation import request_json
-from getjson.mrequests_implementation import mrequest_json
 
 getjson = request_json
-mgetjson = mrequest_json
 
-
-# More on the way, maybe
